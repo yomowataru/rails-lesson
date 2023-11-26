b5334b7303896b2a9da12496b21b8f0737e36091
yomowataru <132421740+yomowataru@users.noreply.github.com>
2023-11-27 00:45:34 +0900

    top、new、indexページ作成

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -1,8 +1,19 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to books_path, notice: "投稿に成功しました"
+    else
+      render :new
+    end
   end
 
   def new
+    @book = Book.new
   end
 
   def show
@@ -12,5 +23,11 @@ class BooksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
